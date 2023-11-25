765afb6c762a7c5fbdf8120055484a64d6e15cd1
祥哥 <1052813257@qq.com>
2023-11-25 18:32:41 +0800

    11-25

---
+++ 11-25/11-25.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+//int main()
+//{
+//	int a = 1;
+//	char* p = (char*)&a;
+//	if (*p==1)
+//	{
+//		printf("Ð¡¶Î\n");
+//	}
+//	else
+//	{
+//		printf("´ó¶Ë\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char a = -1;
+//	signed  char b = -1;
+//	unsigned char c = -1;
+//	printf("a=%d,b=%d,c=%d", a, b, c);
+//	return 0;
+//}
+//int main()
+//{
+//	char a = -128;
+//	printf("%u", a);
+//	return 0;
+//}
+//int main()
+//{
+//	char a = 128;
+//	printf("%u", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = -20;
+//	unsigned int j = 10;
+//	printf("%d\n", i + j);
+//	return 0;
+//}
+//int main()
+//{
+//	unsigned int i;
+//	for ( i = 9; i>=0 ; i++)
+//	{
+//		printf("%u\n", i);
+//	}
+//	return 0;
+//}
+//#include<string.h>
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for ( i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d\n", strlen(a));
+//	return 0;
+//}
