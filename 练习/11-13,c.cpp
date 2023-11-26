5f0fba94b3a10e7c32b6f6deccbb5ecf07262d2d
祥哥 <1052813257@qq.com>
2023-11-26 19:51:19 +0800

    11-26

---
+++ 练习/11-13,c.cpp
@@ -0,0 +1,10 @@
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	scanf("%d",&a);
+	printf("%d", a);
+	return 0;
+
+}
---
+++ 练习/text,c.cpp
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	printf("请输入两个数");
+//	scanf("%d%d", &a ,& b);
+//	int num = a + b;
+//	printf("num=%d", num);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 2 == 1)
+//		{
+//			printf("%d ", i);
+//		}
+//
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int day = 0;
+//	printf("请输入月份；>");
+//	
+//	
+//		scanf("%d", &day);
+//	
+//	switch (day)
+//	{
+//	case 1:
+//			printf("春\n"); break;
+//	case 2:
+//			printf("夏\n"); break;
+//	case 3:
+//			printf("秋\n"); break;
+//	case 4:
+//			printf("冬\n"); break;
+//		
+//	default: printf("输入错误！！！"); break;
+//	}
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int a = 0;
+//	while (a<10)
+//	{
+//		if (a == 5)
+//			break;
+//			//continue;
+//		a++;
+//		printf("%d", a);
+//
+//	}
+//	return 0;
+//}
